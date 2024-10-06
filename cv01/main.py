b0b495f9b3e24deded6a7bb2fcd8b5937d873327
Daniel Nýdrle <nydrledaniel@gmail.com>
2024-10-06 12:06:57 +0200

    fix(cv01): duplicities fix(cv01): photo_counter feat(cv01): archive articles fix(cv01): urls & servix links

--- cv01/main.py
+++ cv01/main.py
@@ -7,7 +7,7 @@ import json
 import sys
 from pymongo.errors import ConnectionFailure
 from db_handler import MONGO_CLIENT
-from scraper_handler import get_articles_from_homepage, process_article
+from scraper_handler import get_articles_from_archive, get_articles_from_homepage, process_article
 
 sys.dont_write_bytecode = True
 
@@ -18,6 +18,9 @@ THREADS = 20
 
 TASKS = []
 PROCESSED_ARTICLES = []
+FINISHED_ARTICLES_URLS = set()
+
+ARCHIVE_COUNTER = 1
 
 # Number of articles needed for saving to file
 SAVE_THRESHOLD = 100
@@ -50,12 +53,18 @@ def save_to_file(data: list, filename: str):
 
 def multithread_scrape():
     """Run multithreaded scraper."""
+    global ARCHIVE_COUNTER
     with ThreadPoolExecutor(max_workers=THREADS) as executor:
         while ARTICLE_LIST or TASKS:
-            if len(ARTICLE_LIST) >= THREADS and len(TASKS) < THREADS:
-                for url in ARTICLE_LIST[:THREADS]:
+            while len(TASKS) <= THREADS:
+                if len(ARTICLE_LIST) <= 10:
+                    ARTICLE_LIST.extend(get_articles_from_archive(ARCHIVE_COUNTER))
+                    ARCHIVE_COUNTER += 1
+                url = ARTICLE_LIST.pop(0)
+                if url not in FINISHED_ARTICLES_URLS:
+                    FINISHED_ARTICLES_URLS.add(url)
+                    print(f"{url} | PA: {len(PROCESSED_ARTICLES)} | FAU: {len(FINISHED_ARTICLES_URLS)}")
                     TASKS.append(executor.submit(process_article, url))
-                    ARTICLE_LIST.remove(url)
             
             for future in as_completed(TASKS):
                 try:
@@ -64,12 +73,22 @@ def multithread_scrape():
                     ARTICLE_LIST.extend(new_articles)
                 except Exception as e:
                     print(f"Error in task: {e}")
-                TASKS.remove(future)
-            if (len(PROCESSED_ARTICLES) > 0) and (len(PROCESSED_ARTICLES) % SAVE_THRESHOLD < THREADS):
-                save_to_file(PROCESSED_ARTICLES, "cv01/articles.json")
+                finally:
+                    TASKS.remove(future)
+            print(f"Finished articles: {len(PROCESSED_ARTICLES)} | Finished urls: {len(FINISHED_ARTICLES_URLS)}")
+            save_to_file(PROCESSED_ARTICLES, "cv01/articles.json")
+            PROCESSED_ARTICLES.clear()
+
+def load_previous_finished_files():
+    """Load previously finished files."""
+    with open("cv01/articles.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+        for article in data:
+            FINISHED_ARTICLES_URLS.add(article["url"])
 
 if __name__ == "__main__":
-    test_connection()
+    # test_connection()
+    load_previous_finished_files()
     init_articles = get_articles_from_homepage()
     ARTICLE_LIST.extend(init_articles)
     multithread_scrape()
--- cv01/scraper_handler.py
+++ cv01/scraper_handler.py
@@ -3,6 +3,8 @@ Scraper handler module.
 """
 
 import json
+from itertools import chain
+from urllib.parse import unquote
 import requests
 import requests.cookies
 from pyquery import PyQuery
@@ -13,7 +15,6 @@ s.cookies.set(name="dCMP", value="mafra=1111,all=1,reklama=1,part=0,cpex=1,googl
 
 def get_page(url: str):
     """Get html page source."""
-    
     res = s.get(url, cookies=s.cookies)
     return res.text
 
@@ -21,20 +22,28 @@ def get_article_urls(html: str):
     """Get article urls from page."""
     pq = PyQuery(html)
     article_urls = pq("[score-type='Article']").items()
+    chain(article_urls, pq("a.art-link").items(), pq("a.aside").items())
     return article_urls
 
-def get_article_data(url, html):
+def get_article_data(url: str, html: str):
     """Parse article data."""
     pq = PyQuery(html)
     
+    title = pq.find("h1").text()
+    text = [pq.find(".opener").text()] + [PyQuery(e).text() for e in pq.find("#art-text p")]
+    tags = [PyQuery(e).text() for e in pq.find("#art-tags > a")]
+    photo_count = int(pq.find(".more-gallery b").text()[0]) if pq.find(".more-gallery b").text() else 0
+    time = pq.find(".art-info .time span.time-date").attr("content")
+    comment_count = int(pq.find("#moot-linkin span").text().split()[0][1:]) if pq.find("#moot-linkin span").text() else 0
+
     data = {
         "url": url,
-        "title": pq.find("h1").text(),
-        "text": [pq.find(".opener").text()] + [PyQuery(e).text() for e in pq.find("#art-text p")],
-        "tags": [PyQuery(e).text() for e in pq.find("#art-tags > a")],
-        "photo_count": int(pq.find(".more-gallery span").text()[0]) if pq.find(".more-gallery span").text() else 0,
-        "time": pq.find(".art-info .time span.time-date").attr("content"),
-        "comment_count": int(pq.find("#moot-linkin span").text().split()[0][1:]) if pq.find("#moot-linkin span").text() else 0,
+        "title": title,
+        "text": text,
+        "tags": tags,
+        "photo_count": photo_count,
+        "time": time,
+        "comment_count": comment_count
     }
 
     if data["text"] is None:
@@ -46,7 +55,13 @@ def get_articles_from_homepage():
     """Get homepage article urls."""
     hp = get_page("https://idnes.cz")
     urls = get_article_urls(hp)
-    return [url.attr("href") for url in urls]
+    return [unquote(url.attr("href")) for url in urls]
+
+def get_articles_from_archive(index: int):
+    """Get archive article urls."""
+    archive = get_page(f"https://www.idnes.cz/zpravy/archiv/{index}")
+    urls = get_article_urls(archive)
+    return [unquote(url.attr("href")) for url in urls]
 
 def process_article(url):
     """
@@ -57,12 +72,13 @@ def process_article(url):
     4) Return list of new article urls
     """
     try:
-        html = get_page(url)
+        html = get_page(unquote(url))
         article_data = get_article_data(url, html)
         json_article_data = json.dumps(article_data, indent=2, ensure_ascii=False)
 
         articles = get_article_urls(html)
-        return [json_article_data, [article.attr("href") for article in articles]]
+        hrefs = [(article.attr("href") if "/_servix/" not in article.attr("href") else article.attr("href").split("url=")[1].split("%3fzdroj")[0]) for article in articles]
+        return [json_article_data, hrefs]
     except Exception as e:
         print(f"Error processing article {url}: {e}")
         return []
