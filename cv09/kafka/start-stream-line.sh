11b2004620da24c961f55165ab81c815ebbe88aa
Daniel Nýdrle <nydrledaniel@gmail.com>
2024-12-16 09:01:37 +0100

    cv09 working kafka after reset with host.docker.internal

--- cv09/kafka/start-stream-line.sh
+++ cv09/kafka/start-stream-line.sh
@@ -4,14 +4,14 @@
 sleep 10
 
 # Create the Kafka topic
-kafka-topics --create --topic test-topic --bootstrap-server kafka:9092 --partitions 1 --replication-factor 1 || true
+kafka-topics --create --topic test-topic --bootstrap-server host.docker.internal:9092 --partitions 1 --replication-factor 1 || true
 
 # Stream lines in an infinite loop
 if [ -f /files/idnes-big.txt ]; then
     while true; do  # Infinite loop to continuously read the file
         echo "Restarting the file stream..."
         while IFS= read -r line; do
-            echo "$line" | kafka-console-producer --broker-list kafka:9092 --topic test-topic
+            echo "$line" | kafka-console-producer --broker-list host.docker.internal:9092 --topic test-topic
             echo "Produced: $line"
             sleep 5  # Delay of 5 seconds between lines
         done < /files/idnes-big.txt
